3cd7946e1adf6398d470beaaf6de6dd7fe91a39d
nguyennam1512001 <nam01883881001@gmail.com>
2023-09-28 22:30:39 +0700

    Refactor code: create routes, app/controllers folders

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+var newRouter = require('./news')
+var siteRouter = require('./site')
+
+function route(app){
+    
+      
+    app.use('/news', newRouter)
+    app.use('/', siteRouter)
+    
+    
+      
+}
+
+module.exports = route
---
+++ src/routes/site.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var router = express.Router()
+
+var siteController = require('../app/controllers/SiteController')
+
+router.use('/search', siteController.search)
+
+router.use('/', siteController.index)
+
+
+module.exports = router
